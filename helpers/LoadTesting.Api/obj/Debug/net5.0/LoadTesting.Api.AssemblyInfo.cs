18a24bc9d69a8e05f454feeab7d6f610503db99a
Azees <abdulaziztoba@outlook.com>
2025-04-17 00:01:05 +0200

    ddd

--- helpers/LoadTesting.Api/obj/Debug/net5.0/LoadTesting.Api.AssemblyInfo.cs
+++ helpers/LoadTesting.Api/obj/Debug/net5.0/LoadTesting.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoadTesting.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1c29246048f1e7caa12c6cbc5ab326031c0220")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoadTesting.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoadTesting.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
